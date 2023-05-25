759800071bf914889aeebb0c2c9485659b473dfb
Nhlalala <nhlalala60@gmail.com>
2023-05-25 15:58:39 +0200

    button

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TakeSurveyComponent } from './take-survey/take-survey.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'take-survey',component:TakeSurveyComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
