5ae3ed576830051d5c2bdfccd7a908a43bad32a5
Nhlalala <nhlalala60@gmail.com>
2023-05-30 08:05:41 +0200

    backend

--- backend/app/routes/questionSurvey.routes.js
+++ backend/app/routes/questionSurvey.routes.js
@@ -1,48 +1,29 @@
 const questionSurvey = require("../controllers/questionSurvey.controller");
 
-module.exports = function(app) {
-    app.use(function(req, res, next) {
-      res.header(
-        "Access-Control-Allow-Headers",
-        "x-access-token, Origin, Content-Type, Accept"
-      );
-      next();
-    });
+module.exports = app => {
+   
   
    
-    // app.get("/api/questionSurvey/", 
-    // controller.findAll);
-  
-    // app.post(
-    //   "/api/questionSurvey/",
-    //   controller.create
-    // );
-    // app.get("/api/questionSurvey/:title", controller.findByTitle);
-    // app.put("/api/questionSurvey/:id", controller.update); 
-    //   app.patch("/api/questionSurvey/:id", controller.update);
-  
-    //   app.delete("/api/questionSurvey/all", controller.deleteAll);
-    //   app.delete("/api/questionSurvey/:id", controller.delete);
     var router = require("express").Router();
 
     // Create a new Survey
-    router.post("/", surveys.create);
+    router.post("/", questionSurvey.create);
 
 
     // Retrieve all Surveys
-    router.get("/", surveys.findAll);
+    router.get("/", questionSurvey.findAll);
 
     // Retrieve a single Survey with id
-    router.get("/:id", surveys.findOne);
+    router.get("/:id", questionSurvey.findOne);
 
     // Update a Survey with id
-    router.put("/:id", surveys.update);
+    router.put("/:id", questionSurvey.update);
 
     // Delete a Surveys with id
-    router.delete("/:id", surveys.delete);
+    router.delete("/:id", questionSurvey.delete);
 
     // Delete all Surveys
-    router.delete("/", surveys.deleteAll);
+    router.delete("/", questionSurvey.deleteAll);
 
     app.use('/api/surveys', router);
   
