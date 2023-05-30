3aa76e0a185ed8a9cd9d82ba31ac03dd8bb360a5
Nhlalala <nhlalala60@gmail.com>
2023-05-30 15:54:19 +0200

    results

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SurveyResultsComponent } from './survey-results/survey-results.component';
 import { TakeSurveyComponent } from './take-survey/take-survey.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
-  {path:'take-survey',component:TakeSurveyComponent}
+  {path:'take-survey',component:TakeSurveyComponent},
+  {path:'survey-result',component:SurveyResultsComponent}
 
 ];
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { TakeSurveyComponent } from './take-survey/take-survey.component';
+import { SurveyResultsComponent } from './survey-results/survey-results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    TakeSurveyComponent
+    TakeSurveyComponent,
+    SurveyResultsComponent
   ],
   imports: [
     BrowserModule,
